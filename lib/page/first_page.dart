c0b6f88c7352af8310c006089d650f7446642bba
zogheibRawad <89975932+Toska003@users.noreply.github.com>
2021-12-14 19:59:59 +0200

    Update first_page.dart

--- lib/page/first_page.dart
+++ lib/page/first_page.dart
@@ -22,7 +22,7 @@ class _FirstPage extends State<FirstPage>  {
       appBar: AppBar(actions: <Widget>[
         IconButton(onPressed: (){
           Navigator.pushNamed(context, '/Settings');
-        }, icon: Icon(Icons.ac_unit_outlined))
+        }, icon: Icon(Icons.account_circle_rounded))
       ],
           title: Text('Forn_App'), centerTitle: true),
       body: Center(

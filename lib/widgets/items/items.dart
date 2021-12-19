bc77557c3ccb05487618f5a9dfb57e5f836bdd03
zogheibRawad <89975932+Toska003@users.noreply.github.com>
2021-12-19 21:44:44 +0200

    Update items.dart

--- lib/widgets/items/items.dart
+++ lib/widgets/items/items.dart
@@ -5,23 +5,28 @@ class FornItem extends StatelessWidget {
   String itemName;
   int itemQty;
 
-  FornItem({required this.itemName,required this.itemQty});
+  FornItem({required this.itemName, required this.itemQty});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(8.0),
+      padding: const EdgeInsets.all(2.5),
       child: Container(
         width: 200,
-        color: globals.blue,
-        padding: EdgeInsets.all(2.0),
+        padding: const EdgeInsets.all(2.0),
+        decoration: BoxDecoration(
+            color: globals.blue, borderRadius: BorderRadius.circular(18.0)),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(itemName),
-            const SizedBox(width: 15,),
+            const SizedBox(
+              width: 15,
+            ),
             const Text('x'),
-            const SizedBox(width: 15,),
+            const SizedBox(
+              width: 15,
+            ),
             Text(itemQty.toString()),
           ],
         ),

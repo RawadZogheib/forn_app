ae51d7ab549095faa42e6e529545647a016fce7b
Michel115 <91220948+Michel115@users.noreply.github.com>
2022-02-16 18:20:24 +0200

    .

--- lib/globals/globals.dart
+++ lib/globals/globals.dart
@@ -11,7 +11,7 @@ import 'package:forn_app/widgets/other/MyToast.dart';
 const String version = "v1.0";
 
 //Server Ip  (page[/my_api.dart])
-const String myIP = "https://kwikcode.net/moonLighters_php/";
+const String myIP = "https://kwikcode.net/forn_php/";
 
 
 //url
